c35faa804cfaf5897afed8c2b4d6168d7b7eecec
Unknown <khakhalin@gmail.com>
2019-04-24 18:16:40 -0400

    troubleshooting velocity calculations

--- track_analysis.R
+++ track_analysis.R
@@ -3,6 +3,7 @@
 require(tidyr)
 require(dplyr)
 require(ggplot2)
+require(viridis)
 
 folderName <- "C:/Users/Arseny/Documents/2_Behavior/behav-git/data/"
 
@@ -21,9 +22,6 @@ dd <- d[-1,]-d[1:(nTime-1),] # Differences dataframe
 dd$step <- dd$time
 dd$time <- d$time[1:(nTime-1)]
 
-# Plot trajectories for 1 tadpole:
-ggplot(data=d,aes(x1,y1)) + geom_path() + theme_classic() # geom_path for trajectories!
-
 head(d)
 d <- d[1:(nTime-1),] # Shorten, to make same length as dd
 # ggplot(data=d,aes(time,a1)) + geom_line() + theme_classic() # Accuracy
@@ -32,22 +30,27 @@ dcopy <- d # Just in case I mess upp
 d <- dcopy # Restore
 
 tads <- list() # Init empty list
-for(iTad in 1:5){
+for(iTad in 1){ # normally would be in 1:5
   print(iTad)
   #dd <- dd %>% mutate(temp=sqrt(.[[3*(iTad-1)+2]]^2 + .[[3*(iTad-1)+3]]^2)/step*1000) # Calculate speed
   #d <- cbind(d,dd$temp) # Move this column to d
   #names(d)[16+iTad] <- paste0("s",iTad) # Name it s1..s5
   
-  tads[[iTad]] <- data.frame(time=d$time, a=d[[3*(iTad-1)+1]],
+  tads[[iTad]] <- data.frame(time=d$time, a=d[[3*(iTad-1)+4]],
                              x=d[[3*(iTad-1)+2]], y=d[[3*(iTad-1)+3]],
                              vx=dd[[3*(iTad-1)+2]]/dd$step, vy=dd[[3*(iTad-1)+3]]/dd$step)
   tads[[iTad]] <- mutate(tads[[iTad]], v=sqrt(vx^2 + vy^2))
   
   p <- ggplot() + theme_classic()
   for(t in 1:nTime){
-    if(!is.na(tads[[iTad]]$v[t]) & t>3 & t<nTime-20){
-      if(tads[[iTad]]$v[t]>0.5){
-        p <- p + geom_path(data=data.frame(x=-3:10, y=tads[[iTad]]$v[(t-3):(t+20)]), aes(x,y))
+    if(!is.na(tads[[iTad]]$v[t])){
+      if(tads[[iTad]]$a[t]<0.75){
+        tads[[iTad]]$v[t] <- (tads[[iTad]]$v[t-1] + tads[[iTad]]$v[t+1])/2
+      }
+      if(t>3 & t<nTime-20){
+        if(tads[[iTad]]$v[t]>0.25){
+          p <- p + geom_path(data=data.frame(x=-3:20, y=tads[[iTad]]$v[(t-3):(t+20)]), aes(x,y), alpha=0.5)
+        }
       }
     }
   }
@@ -59,8 +62,11 @@ for(iTad in 1:5){
     }
   }
 }
-head(tads[[3]])
+head(tads[[1]])
 
-
-ggplot(tads[[3]],aes(a,v)) + geom_jitter(alpha=0.2,w=0.02,h=0) + theme_classic() + 
+ggplot(tads[[1]],aes(a,v)) + geom_jitter(alpha=0.2,w=0.02,h=0) + theme_classic() + 
   geom_smooth(method="lm",se=F)
+
+# Plot trajectories for 1 tadpole:
+ggplot(data=tads[[1]],aes(x,y,color=v)) + geom_path() + # it's good to use geom_path for trajectories!
+  theme_classic() + scale_colour_viridis()
