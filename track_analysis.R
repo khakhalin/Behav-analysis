3451d7155008618d49c2cd2e32538fe608f81042
Unknown <khakhalin@gmail.com>
2019-04-26 18:24:19 -0400

    track analysis: + schooling snapshots

--- track_analysis.R
+++ track_analysis.R
@@ -7,10 +7,12 @@ require(viridis)
 
 rm(list=ls()) # Clear workspace
 
+do_basics = 0 # Whether we need to calculate per-tadpole values, or go straight to schooling
+
 folderName <- "C:/Users/Arseny/Documents/2_Behavior/behav-git/data/" # Work
 # folderName <- "C:/Users/Sysadmin/Documents/_Science/Behav-analysis-git/data/"
 
-expName = 'Control4'
+expName = 'Control1'
 d <- read.csv(paste(folderName,expName,'.csv',sep=""))
 # headers: time,x1,y1,a1,x2,y2,a2,x3,y3,a3,x4,y4,a4,x5,y5,a5,
 
@@ -50,63 +52,120 @@ dishDiameter
 dishCenter <- c(mean(c(dishSides[1],dishSides[2])),mean(c(dishSides[3],dishSides[4])))
 dishCenter
 
-# Plot trajectory for 1 tadpole:
-ggplot(data=tads[[2]],aes(x,y,color=v)) + geom_path() + # it's good to use geom_path for trajectories!
-  theme_void() + scale_colour_viridis() + coord_fixed(ratio=1)
-last_plot() + geom_point(data=data.frame(x=dishCenter[1],y=dishCenter[2]),aes(x,y),color="red")
-
-
-### --- Average speeds
-out <- data.frame(expName=rep(expName,5),id=1:5) # Initialize output dataframe
-newSlice <- data.frame()
-for(iTad in 1:5){
-  newSlice <- rbind(newSlice, tads[[iTad]] %>% filter(!is.na(v)) %>% filter(a>0.5) %>% summarize(v=mean(v)))
-  # Technically a before should also be good, for v estimation to be decent
+if(do_basics){
+  # Plot trajectory for 1 tadpole:
+  ggplot(data=tads[[2]],aes(x,y,color=v)) + geom_path() + # it's good to use geom_path for trajectories!
+    theme_void() + scale_colour_viridis() + coord_fixed(ratio=1)
+  last_plot() + geom_point(data=data.frame(x=dishCenter[1],y=dishCenter[2]),aes(x,y),color="red")
+  
+  
+  ### --- Average speeds
+  out <- data.frame(expName=rep(expName,5),id=1:5) # Initialize output dataframe
+  newSlice <- data.frame()
+  for(iTad in 1:5){
+    newSlice <- rbind(newSlice, tads[[iTad]] %>% filter(!is.na(v)) %>% filter(a>0.5) %>% summarize(v=mean(v)))
+    # Technically a before should also be good, for v estimation to be decent
+  }
+  out <- cbind(out,newSlice)
+  out$v <- out$v*1000/dishDiameter*14 # from pixels/ms to cm/s
+  
+  
+  ### --- Share of time spent in the middle (within 2/3 r from the center)
+  newSlice <- data.frame()
+  for(iTad in 1:5){
+    temp <- tads[[iTad]] %>% filter(a>0.5) # Only leave good points
+    newSlice <- rbind(newSlice, data.frame(r=mean(
+      sqrt((temp$x-dishCenter[1])^2 + (temp$y-dishCenter[2])^2) < dishDiameter/3)))
+  }
+  #newSlice
+  out <- cbind(out,newSlice)
+  #out
+  
+  
+  ### --- Tadpole-tadpole distances, calculated per tadpole
+  dist <- data.frame()
+  for(iTad in 1:5){
+    if(iTad>1){ # Because for loops in R are weird, need this extra check
+      tempi <- tads[[iTad]][,1:4] # Only first 4 columns
+      names(tempi) <- c("t1","a1","x1","y1") # Rename
+      for(jTad in 1:(iTad-1)){
+        temp <- cbind(tempi,tads[[jTad]][,1:4]) # Add second tad with original column names
+        temp <- temp %>% filter(a>0.5 & a1>0.5) # Only leave reasonable points
+        dist <- rbind(dist, summarize(temp, i=iTad, j=jTad, 
+                                      dm=mean(sqrt((x-x1)^2 + (y-y1)^2)),
+                                      ds = sd(sqrt((x-x1)^2 + (y-y1)^2))))
+      }
+    }
+  }
+  # dist
+  newSlice <- data.frame()
+  for(iTad in 1:5){
+    temp <- rbind(filter(dist,i==iTad),filter(dist,j==iTad))
+    newSlice <- rbind(newSlice, summarize(temp, dm=mean(dm), ds=mean(ds)))
+  }
+  # newSlice
+  out <- cbind(out,newSlice)
+  out <- mutate(out, dm = dm/dishDiameter*14, ds = ds/dishDiameter*14) # From pixels to cm
+  
+  print(out, row.names=FALSE) # <-- Main output
+  
+  stop() # Don't execute everything after
 }
-out <- cbind(out,newSlice)
-out$v <- out$v*1000/dishDiameter*14 # from pixels/ms to cm/s
 
 
-### --- Share of time spent in the middle (within 2/3 r from the center)
-newSlice <- data.frame()
-for(iTad in 1:5){
-  temp <- tads[[iTad]] %>% filter(a>0.5) # Only leave good points
-  newSlice <- rbind(newSlice, data.frame(r=mean(
-    sqrt((temp$x-dishCenter[1])^2 + (temp$y-dishCenter[2])^2) < dishDiameter/3)))
-}
-#newSlice
-out <- cbind(out,newSlice)
-#out
-
-
-### --- Tadpole-tadpole distances
-dist <- data.frame()
+### ------------------ Schooling assessment
+# Part one: calculate the distances
+dfDist <- data.frame()
 for(iTad in 1:5){
   if(iTad>1){ # Because for loops in R are weird, need this extra check
     tempi <- tads[[iTad]][,1:4] # Only first 4 columns
     names(tempi) <- c("t1","a1","x1","y1") # Rename
     for(jTad in 1:(iTad-1)){
       temp <- cbind(tempi,tads[[jTad]][,1:4]) # Add second tad with original column names
-      temp <- temp %>% filter(a>0.5 & a1>0.5) # Only leave reasonable points
-      dist <- rbind(dist, summarize(temp, i=iTad, j=jTad, 
-                                    dm=mean(sqrt((x-x1)^2 + (y-y1)^2)),
-                                    ds = sd(sqrt((x-x1)^2 + (y-y1)^2))))
+      temp <- cbind(temp,with(temp,sqrt((x-x1)^2 + (y-y1)^2))) # Calculate distances
+      names(temp)[length(names(temp))]<-"d"  # Rename last column
+      if(nrow(dfDist)==0)
+        dfDist <- data.frame(d=temp$d)
+      else
+        dfDist <- cbind(dfDist, temp$d)
+      names(dfDist)[length(names(dfDist))]<-sprintf("c%d%d",iTad,jTad)  # Rename last column
     }
   }
 }
-# dist
-newSlice <- data.frame()
+#head(dfDist)
+
+# Part two: calculate certainties
 for(iTad in 1:5){
-  temp <- rbind(filter(dist,i==iTad),filter(dist,j==iTad))
-  newSlice <- rbind(newSlice, summarize(temp, dm=mean(dm), ds=mean(ds)))
+  if(iTad==1)
+    afull <- data.frame(time=tads[[iTad]]$time, a=tads[[iTad]]$a)
+  else
+    afull <- cbind(afull,tads[[iTad]]$a)
+  names(afull)[length(names(afull))]<-sprintf("a%d",iTad)
 }
-# newSlice
-out <- cbind(out,newSlice)
-out <- mutate(out, dm = dm/dishDiameter*14, ds = ds/dishDiameter*14) # From pixels to cm
+afull <- afull %>% mutate(a=a1*a2*a3*a4*a5) # Sort of total cumulative certainty
 
-print(out, row.names=FALSE) # <-- Main output
+# Part 3: Sample at good frames
+# Time is measured in ms and runs ro about 11 seconds. Let's take measurements every 2 minutes.
+maxTime <- tads[[1]]$time[nrow(tads[[1]])]
+schooling <- data.frame() # We'll be collecting data here
+for(iFrame in 0:5){
+  currentTime <- afull$time[1]+iFrame*60*1000*2
+  currentFrame <- min(which(afull$time>currentTime & afull$a>0.8)) # Find closest certain frame
+  if(currentFrame <= maxTime){ # We are still within the video
+    print(sprintf("%d",currentFrame))
+    temp <- data.frame(d=as.numeric(dfDist[currentFrame,])) # Select a row, transpose into a column
+    schooling <- rbind(schooling, summarize(temp,m=mean(d),s=sd(d)))
+  }
+}
+schooling <- schooling %>% mutate(m=m/dishDiameter*14, s=s/dishDiameter*14)
+schooling <- cbind(data.frame(name=rep(expName,nrow(schooling))),schooling)
 
-stop() # Don't execute everything after
+
+print(schooling, row.names=FALSE) # Main output for this section
+
+stop() # Don't execute further
+
+
 
 
 ### ------- Assorted visual tests
