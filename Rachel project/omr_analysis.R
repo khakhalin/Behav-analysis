143628b93ce60b8339739f5f6f73c8c0a46331e9
khakhalin <khakhalin@gmail.com>
2020-04-07 14:53:04 -0400

    + nested analysis

--- Rachel project/omr_analysis.R
+++ Rachel project/omr_analysis.R
@@ -48,7 +48,11 @@ model = aov(data=ds, Change~Treatment)
 summary(model)
 TukeyHSD(model) # Not surprising, witn n=4...
 
-# A bit incorrect analysis
+# But is there a total effect?
+t.test(ds$Change)
+
+
+# -------- A bit incorrect analysis (each run becomes a point)
 d <- d %>% mutate(Change = (After-Before)/(5-Before))
 summary(d)
 
@@ -60,3 +64,12 @@ ggplot(data=d,aes(Treatment,Change, color=Treatment)) +
 model = aov(data=d, Change~Treatment)
 summary(model)
 TukeyHSD(model) # Same thing. Good to know.
+
+# -------- Bad nested analysis (account for within-group variation)
+ggplot(data=d,aes(Treatment,Change, color=factor(Group))) + 
+  theme_classic() + geom_hline(yintercept=0, color='gray') +
+  geom_jitter(height=0.1, width=0.1) +
+  ylab('Mean movement')
+model = aov(data=d, Change~Treatment+factor(Group))
+summary(model)
+TukeyHSD(model, which='Treatment') # As expected, no effect
